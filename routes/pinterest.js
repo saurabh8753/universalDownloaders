0c6859800820e8c7087c93e44eea82b4146f6ff4
Skzbots <130946713+saurabh8753@users.noreply.github.com>
2026-03-09 21:11:27 +0530

    Update pinterest.js

--- routes/pinterest.js
+++ routes/pinterest.js
@@ -1,8 +1,61 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
 
-const { handlePinterestDownload } = require("../controllers/pinterestController");
+const {
+  handlePinterestDownload,
+} = require("../controllers/pinterestController");
 
+
+// MAIN API
 router.get("/download", handlePinterestDownload);
+
+
+// FORCE DOWNLOAD PROXY
+router.get("/file", async (req, res) => {
+
+  try {
+
+    const { url } = req.query;
+
+    if (!url) {
+
+      return res.status(400).json({
+        success:false,
+        error:"Missing url parameter"
+      });
+
+    }
+
+    const response = await axios.get(url,{
+      responseType:"stream"
+    });
+
+    const fileName = url.split("/").pop().split("?")[0];
+
+    res.setHeader(
+      "Content-Disposition",
+      `attachment; filename="${fileName}"`
+    );
+
+    res.setHeader(
+      "Content-Type",
+      response.headers["content-type"]
+    );
+
+    response.data.pipe(res);
+
+  }
+
+  catch(err){
+
+    res.status(500).json({
+      success:false,
+      error:"Download failed"
+    });
+
+  }
+
+});
 
 module.exports = router;
