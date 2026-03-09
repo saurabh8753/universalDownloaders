9b6c543180422ece3e5bd4d62cd3808e176e4436
Skzbots <130946713+saurabh8753@users.noreply.github.com>
2026-03-09 14:36:28 +0530

    Update pinterest.js

--- routes/pinterest.js
+++ routes/pinterest.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const {
   handlePinterestDownload,
+  handleProxyDownload, // Ise import karein
 } = require("../controllers/pinterestController");
 
 router.get("/download", handlePinterestDownload);
+router.get("/proxy-file", handleProxyDownload); // Naya endpoint
 
 module.exports = router;
